06016cc0f3e03b4464b257c6b3ace510eb484d04
Ryan DeCook <ryandecook@gmail.com>
2017-12-15 15:44:30 -0600

    add repos view

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,15 +1,14 @@
 var myApp = angular.module('myApp', ['ngRoute']);
 
 /// Routes ///
-myApp.config(function($routeProvider, $locationProvider) {
-  $locationProvider.hashPrefix('');
-
-  $routeProvider
-    .when('/', {
+myApp.config(function($routeProvider) {
+  $routeProvider.when('/', {
       templateUrl: '/views/user.html',
       controller: 'UserController as uc',
     }).when('/repos', {
-      templateUrl: '/views/repose.html',
+      templateUrl: '/views/repos.html',
       controller: 'UserController as uc'
-    });
+    }).otherwise({
+      template: '<h1>404</h1>'
+  });
 });
